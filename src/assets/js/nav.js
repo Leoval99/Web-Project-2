ad7524ca5d9eee8e74e202e184d1d5293116c0ab
Leoval99 <leoval99@gmail.com>
2021-08-11 15:56:20 -0300

    json creado

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,11 +1,6 @@
-const link = document.querySelector("#link")
 btn.addEventListener("click", () => {
     lista.classList.toggle("mostrar");
 });
 btn.addEventListener("click", () => {
     btn.classList.toggle("btn-1");
 });
-
-link.addEventListener("click", () =>{
-    lista.classList.toggle("ocultar");
-})
