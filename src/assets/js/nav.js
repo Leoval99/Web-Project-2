e13add8a010cefdbe7157562e1ad053b56e306e8
Leoval99 <leoval99@gmail.com>
2021-08-10 21:14:49 -0300

    contacto terminado

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,6 +1,11 @@
+const link = document.querySelector("#link")
 btn.addEventListener("click", () => {
     lista.classList.toggle("mostrar");
 });
 btn.addEventListener("click", () => {
     btn.classList.toggle("btn-1");
 });
+
+link.addEventListener("click", () =>{
+    lista.classList.toggle("ocultar");
+})
