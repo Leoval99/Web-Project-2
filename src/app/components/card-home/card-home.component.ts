188becfdab92adcd4cfbf642542475367f270637
Leoval99 <leoval99@gmail.com>
2021-08-12 22:44:57 -0300

    Whats funciona

--- src/app/components/card-home/card-home.component.ts
+++ src/app/components/card-home/card-home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import productos from '../../../assets/json/productos.json'
+
 @Component({
   selector: 'app-card-home',
   templateUrl: './card-home.component.html',
   styleUrls: ['./card-home.component.css']
 })
 export class CardHomeComponent implements OnInit {
+
+  productList:{url:null,titulo:string,descripcion:String,precio:[String]}[] = productos;
 
   constructor() { }
  
