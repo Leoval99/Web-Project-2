ee2b6f8a7809c975a7002e87c7e3442d76827fd7
EZEQUIEL-CHOQUE <ezequielchoque0@gmail.com>
2021-08-13 19:52:06 -0300

    Cambie el orden de carga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ import productos from '../assets/json/productos.json';
 export class AppComponent {
   title = 'final';
   productList:{titulo:string,descripcion:String,precio:[String]}[] = productos;
+  myScriptElement:HTMLScriptElement;
+  constructor(){
+    this.myScriptElement=document.createElement("script");
+    this.myScriptElement.src ="https://cdn.jsdelivr.net/npm/keen-slider@5.5.0/keen-slider.min.js";
+    document.body.appendChild(this.myScriptElement);
+  }
 }
--- src/app/components/slider-home/slider-home.component.ts
+++ src/app/components/slider-home/slider-home.component.ts
@@ -7,15 +7,9 @@ import { CargarScriptsService } from 'src/app/cargar-scripts.service';
   styleUrls: ['./slider-home.component.css']
 })
 export class SliderHomeComponent implements OnInit {
-
-  myScriptElement:HTMLScriptElement;
-
+  
   constructor(private _CargarScripts:CargarScriptsService) {
     _CargarScripts.carga(["slider-home"]);
-
-    this.myScriptElement=document.createElement("script");
-    this.myScriptElement.src ="https://cdn.jsdelivr.net/npm/keen-slider@5.5.0/keen-slider.min.js";
-    document.body.appendChild(this.myScriptElement);
   }
 
   ngOnInit(): void {
