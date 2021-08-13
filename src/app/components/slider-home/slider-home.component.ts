d295bf5afd97c263eb3058e1700ca2d7b90fc33e
EZEQUIEL-CHOQUE <ezequielchoque0@gmail.com>
2021-08-13 19:34:11 -0300

    Probando Slider externo 😋

--- src/app/components/slider-home/slider-home.component.ts
+++ src/app/components/slider-home/slider-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CargarScriptsService } from 'src/app/cargar-scripts.service';
 
 @Component({
   selector: 'app-slider-home',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderHomeComponent implements OnInit {
 
-  constructor() {
+  myScriptElement:HTMLScriptElement;
+
+  constructor(private _CargarScripts:CargarScriptsService) {
+    _CargarScripts.carga(["slider-home"]);
+
+    this.myScriptElement=document.createElement("script");
+    this.myScriptElement.src ="https://cdn.jsdelivr.net/npm/keen-slider@5.5.0/keen-slider.min.js";
+    document.body.appendChild(this.myScriptElement);
   }
 
   ngOnInit(): void {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  myScriptElement:HTMLScriptElement;
 
-  constructor() { }
+  constructor() {
+    this.myScriptElement=document.createElement("script");
+    this.myScriptElement.src ="https://cdn.jsdelivr.net/npm/keen-slider@5.5.0/keen-slider.min.js";
+    document.body.appendChild(this.myScriptElement);
+   }
 
   titulo = "Productos destacados";
 
